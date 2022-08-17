446b0da576ac340310099200f14798843b94156d
DannyHep <dannyhep@hotmail.co.uk>
2022-08-17 14:47:04 +0100

    changed route to patient aide backend to new route

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
 import { router } from "./routes/user.js";
 import "dotenv/config";
 
@@ -9,8 +8,6 @@ const port = process.env.PORT || 8088;
 
 app.use(express.json());
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 // routes
 app.use("/user", router);
