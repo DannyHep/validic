7007544a592b2a792f036dc321cc34329ae0ea39
Rafael Lima <rafauxdev@gmail.com>
2022-10-25 12:01:22 +0100

    Add authenticateUser as middleware to the protected routes

--- routes/user.js
+++ routes/user.js
@@ -6,12 +6,15 @@ import {
   parsedTrackerData,
   addTrackerData,
 } from "../controllers/user.js";
+import authenticateUser from "../middleware/authenticateUser.js";
 
 const router = express.Router();
 
 // router.get("/my-health", getAllUsers);
 // router.post("/validic-fitness-data", getValidicFitnessData);
 router.post("/createHealthTrackerValues", createTrackerMeasurements); // this route doesn't need jwt protect
+
+router.use(authenticateUser)
 router.post("/create-validic-profile", createNewUser);
 router.post("/validic-profile", getValidicProfile);
 router.post("/getTrackerMeasurements", parsedTrackerData);
