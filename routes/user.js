7e5267c1c9f6c9a6bbfca648937f03b70ba8bb05
Rafael Lima <rafauxdev@gmail.com>
2022-10-25 12:00:56 +0100

    Move unprotected route to top createHealthTrackerValues

--- routes/user.js
+++ routes/user.js
@@ -11,9 +11,9 @@ const router = express.Router();
 
 // router.get("/my-health", getAllUsers);
 // router.post("/validic-fitness-data", getValidicFitnessData);
+router.post("/createHealthTrackerValues", createTrackerMeasurements); // this route doesn't need jwt protect
 router.post("/create-validic-profile", createNewUser);
 router.post("/validic-profile", getValidicProfile);
-router.post("/createHealthTrackerValues", createTrackerMeasurements);
 router.post("/getTrackerMeasurements", parsedTrackerData);
 router.put("/addTrackerData", addTrackerData);
 
